cb688aa12010ed2c4b6cc7271539c62468ecf385
Mark Shuler <biggraydog@live.com>
2016-10-17 16:39:09 -0500

    works, but no guess counter yet

--- hangman.js
+++ hangman.js
@@ -1,12 +1,35 @@
-var words = ["hangman", "javascript", "house", "dog", "cat", "monkey", "table", "chair", "coffee", "island", "mirror", "ottoman", "fountain", "feature", "water", "red", "green", "blue", "hypertext", "markup", "language"];
+var words = ["hangman", "javascript", "house", "monkey", "table", "chair", "coffee", "island", "mirror", "ottoman", "fountain", "feature", "water", "beginning", "green", "tailor", "hypertext", "markup", "language", "captain"];
 
 var word = words[Math.floor(Math.random() * words.length)];
 
 var AnswerArray = [];
-  for (var i=0, i < word.length, i++){
+  for (var i = 0; i < word.length; i++){
     AnswerArray[i] = "_";
   }
 
   var remainingLetters = word.length;
 
+  while (remainingLetters > 0) {
+
+    alert(AnswerArray.join(" "));
+
+    var guess = prompt("Guess a letter, or click Cancel to stop playing.");
+    if (guess === null) {
+      break;
+    } else if (guess.length !== 1) {
+      alert("Please enter only one letter");
+    }else {
+      for (var j = 0; j < word.length; j++){
+        if (word[j] === guess) {
+          AnswerArray[j] = guess;
+          remainingLetters--;
+        }
+      }
+    }
+
+
+  }
+
+
   alert(AnswerArray.join(" "));
+  alert("Good Job! The answer was " + word);
